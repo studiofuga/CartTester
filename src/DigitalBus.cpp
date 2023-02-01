e959034c8ad58624b1725d8458d950e040d0ee5e
Federico Fuga <fuga@studiofuga.com>
2023-02-01 16:08:26 +0100

    Fix BUS Pin Mapping.
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- src/DigitalBus.cpp
+++ src/DigitalBus.cpp
@@ -6,11 +6,11 @@
 #include "Arduino.h"
 
 const uint8_t DigitalBus::AddressPin[13] = {
-    45, 43, 41, 39, 37, 35, 33, 31, 34, 36, 38, 40, 42
+    44, 42, 40, 38, 36, 34, 32, 30, 35, 37, 39, 41, 43
 };
 
 const uint8_t DigitalBus::DataPin[8] = {
-    47, 49, 51, 52, 50, 48, 46, 44
+    46, 48, 50, 53, 51, 49, 47, 45
 };
 
 DigitalBus::DigitalBus() = default;
